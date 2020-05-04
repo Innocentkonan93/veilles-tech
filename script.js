de2e9fb498d4f2f80ee5e62ebcdb82009c807a8a
Konan Koffi Innocent <63507839+Innocentkonan93@users.noreply.github.com>
2020-05-04 13:42:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+var num = 10;
+
+var montant = 100;
+var myCote1 = 1.29;
+var myCote2 = 3.49;
+var somme1 = montant/myCote1;
+var somme2 = montant - somme1;
+
+
+
+function changeVariableValue()
+{
+   	num = 11;
+  	myCalcul1 = somme1 * myCote1;
+	myCalcul2 = somme2 * myCote2;
+
+	myCalcul3 = (somme1 - 1) * myCote1;
+	myCalcul4 = (somme2 + 1) * myCote2;
+
+	myCalcul5 = (somme1 - 2) * myCote1;
+	myCalcul6 = (somme2 + 2) * myCote2;
+
+
+	myCalcul7 = (somme1 - 3) * myCote1;
+	myCalcul8 = (somme2 + 3) * myCote2;
+
+	myCalcul9 = (somme1 - 4) * myCote1;
+	myCalcul10 = (somme2 + 4) * myCote2;
+
+	myCalcul11 = (somme1 - 5) * myCote1;
+  	myCalcul12 = (somme2 + 5) * myCote2;
+  
+  	myCalcul13 = (somme1 - 6) * myCote1;
+  	myCalcul14 = (somme2 + 6) * myCote2;
+  
+  	myCalcul15 = (somme1 - 7) * myCote1;
+  	myCalcul16 = (somme2 + 7) * myCote2;
+  
+  	myCalcul17 = (somme1 - 8) * myCote1;
+  	myCalcul18 = (somme2 + 8) * myCote2;
+  
+	myCalcul19 = (somme1 - 9) * myCote1;
+  	myCalcul20 = (somme2 + 9) * myCote2;
+}
+changeVariableValue();
+
+console.log("num 1: "+ myCalcul1); 
+console.log("num 2: "+ myCalcul2);
+
+console.log("num 1: "+ myCalcul3);
+console.log("num 2: "+ myCalcul4);
+console.log("num 1: "+ myCalcul5);
+console.log("num 2: "+ myCalcul6);
+console.log("num 1: "+ myCalcul7);
+console.log("num 2: "+ myCalcul8);
+console.log("num 1: "+ myCalcul9);
+console.log("num 2: "+ myCalcul10);
+console.log("num 1: "+ myCalcul11);
+console.log("num 2: "+ myCalcul12);
+console.log("num 1: "+ myCalcul13);
+console.log("num 2: "+ myCalcul14);
+console.log("num 1: "+ myCalcul15);
+console.log("num 2: "+ myCalcul16);
+console.log("num 1: "+ myCalcul17);
+console.log("num 2: "+ myCalcul18);
+console.log("num 1: "+ myCalcul19);
+console.log("num 2: "+ myCalcul20);
+
